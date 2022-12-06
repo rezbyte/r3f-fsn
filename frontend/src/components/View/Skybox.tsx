73a2f7fa280206b0f7874d74442a5afebb18da00
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-12-06 13:15:34 +0800

    Fixed camera exiting skybox

--- frontend/src/components/View/Skybox.tsx
+++ frontend/src/components/View/Skybox.tsx
@@ -1,14 +1,15 @@
-import * as THREE from "three";
 import { useRef } from "react";
+import { BackSide } from "three";
 import fragmentShader from "../../shaders/Skybox.glsl";
 
 export default function SkyBox(props: JSX.IntrinsicElements["mesh"]) {
   const mesh = useRef<THREE.Mesh>(null!);
+
   return (
-    <mesh {...props} ref={mesh} scale={10} position={[0, 0, 0]}>
+    <mesh ref={mesh} scale={100} position={[0, 0, 0]} {...props}>
       <boxGeometry args={[1, 1, 1]} />
       <shaderMaterial
-        side={THREE.BackSide}
+        side={BackSide}
         fragmentShader={fragmentShader}
         uniforms={{ resolution: { value: [1, 2] } }}
       />
